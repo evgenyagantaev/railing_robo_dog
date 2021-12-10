624da266c1dd9b4aebbfe8d0a4969e2514673c33
evgenyagantaev <agantaev@mail.ru>
2021-12-10 20:34:19 +0300

    tech;

--- I_Robot.hpp
+++ I_Robot.hpp
@@ -4,4 +4,13 @@ class I_Robot
 {
 	bool turn_on() = 0;
 	bool turn_off() = 0;
+	bool action() = 0;
+	bool stop() = 0;
+	bool forward() = 0;
+	bool faster() = 0;
+	bool slower() = 0;
+	bool move_right() = 0;
+	bool turn_right() = 0;
+	bool move_right() = 0;
+	bool turn_right() = 0;
 }
