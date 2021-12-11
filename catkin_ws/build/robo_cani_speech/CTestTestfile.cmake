5bfa4173721c30dce44243f90508d1ba47857657
evgenyagantaev <agantaev@mail.ru>
2021-12-11 19:43:43 +0300

    tech;

---
+++ catkin_ws/build/robo_cani_speech/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/oem/workspace/ROS/robo_canicross/catkin_ws/src/robo_cani_speech
+# Build directory: /home/oem/workspace/ROS/robo_canicross/catkin_ws/build/robo_cani_speech
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
