aa7c9d656012edfde6544ce6f5206ad4d587e447
evgenyagantaev <agantaev@mail.ru>
2021-12-12 18:26:45 +0300

    can't import Decoder;

--- catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/oem/workspace/ROS/robo_canicross/catkin_ws/src
-# Build directory: /home/oem/workspace/ROS/robo_canicross/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("robo_cani_speech")
