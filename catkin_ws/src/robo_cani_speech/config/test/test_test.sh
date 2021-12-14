f57af760103eabcee2b23881394006634740366c
evgenyagantaev <agantaev@mail.ru>
2021-12-14 09:14:39 +0300

    tech;

---
+++ catkin_ws/src/robo_cani_speech/config/test/test_test.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# This is a simple example of the decoder invocation
+# Please note, that recognize the file should be single-channel with sample rate 8kHz
+# decoder-test.wav: RIFF (little-endian) data, WAVE audio, Microsoft PCM, 16 bit, mono 8000 Hz
+# Expected result for the test file is: илья ильф евгений петров золотой телёнок
+
+pocketsphinx_continuous \
+    -samprate 8000 \
+    -kws test_kwslist.kwslist \
+    -dict test_dictionary.dic \
+    -hmm zero_ru.cd_cont_4000 \
+    -remove_noise no \
+    -infile test_test.wav
